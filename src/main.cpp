98ff34a40413b0dc53af549f2c8c07bc93f061ab
Pit Förster <p.foerster@sealog.de>
2024-01-11 14:45:04 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,171 @@
+/*
+ * HelTec Automation(TM) WIFI_LoRa_32 factory test code, witch includ
+ * follow functions:
+ * 
+ * - Basic OLED function test;
+ * 
+ * - Basic serial port test(in baud rate 115200);
+ * 
+ * - LED blink test;
+ * 
+ * - WIFI connect and scan test;
+ * 
+ * - LoRa Ping-Pong test (DIO0 -- GPIO26 interrup check the new incoming messages);
+ * 
+ * - Timer test and some other Arduino basic functions.
+ *
+ * by Aaron.Lee from HelTec AutoMation, ChengDu, China
+ * 成都惠利特自动化科技有限公司
+ * https://heltec.org
+ *
+ * this project also realess in GitHub:
+ * https://github.com/HelTecAutomation/Heltec_ESP32
+*/
+
+#include "Arduino.h"
+#include "WiFi.h"
+#include "images.h"
+#include "LoRaWan_APP.h"
+#include <Wire.h>  
+#include "HT_SSD1306Wire.h"
+#include "LoRaWan_APP.h"
+
+/* OTAA para*/
+uint8_t devEui[] = { 0x70, 0xB3, 0xD5, 0x7E, 0xD0, 0x06, 0x41, 0x55 };
+uint8_t appEui[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+uint8_t appKey[] = { 0x9C, 0x37, 0xD5, 0x10, 0x1B, 0x63, 0x1D, 0x1A, 0x13, 0x17, 0x8C, 0x5E, 0x6B, 0x62, 0xC0, 0x73 };
+
+/* ABP para*/
+uint8_t nwkSKey[] = { 0x15, 0xb1, 0xd0, 0xef, 0xa4, 0x63, 0xdf, 0xbe, 0x3d, 0x11, 0x18, 0x1e, 0x1e, 0xc7, 0xda,0x85 };
+uint8_t appSKey[] = { 0xd7, 0x2c, 0x78, 0x75, 0x8c, 0xdc, 0xca, 0xbf, 0x55, 0xee, 0x4a, 0x77, 0x8d, 0x16, 0xef,0x67 };
+uint32_t devAddr =  ( uint32_t )0x007e6ae1;
+
+/*LoraWan channelsmask*/
+uint16_t userChannelsMask[6]={ 0x00FF,0x0000,0x0000,0x0000,0x0000,0x0000 };
+
+/*LoraWan region, select in arduino IDE tools*/
+LoRaMacRegion_t loraWanRegion = ACTIVE_REGION;
+
+/*LoraWan Class, Class A and Class C are supported*/
+DeviceClass_t  loraWanClass = CLASS_A;
+
+/*the application data transmission duty cycle.  value in [ms].*/
+uint32_t appTxDutyCycle = 15000;
+
+/*OTAA or ABP*/
+bool overTheAirActivation = true;
+
+/*ADR enable*/
+bool loraWanAdr = true;
+
+
+/* Indicates if the node is sending confirmed or unconfirmed messages */
+bool isTxConfirmed = true;
+
+/* Application port */
+uint8_t appPort = 2;
+/*!
+* Number of trials to transmit the frame, if the LoRaMAC layer did not
+* receive an acknowledgment. The MAC performs a datarate adaptation,
+* according to the LoRaWAN Specification V1.0.2, chapter 18.4, according
+* to the following table:
+*
+* Transmission nb | Data Rate
+* ----------------|-----------
+* 1 (first)       | DR
+* 2               | DR
+* 3               | max(DR-1,0)
+* 4               | max(DR-1,0)
+* 5               | max(DR-2,0)
+* 6               | max(DR-2,0)
+* 7               | max(DR-3,0)
+* 8               | max(DR-3,0)
+*
+* Note, that if NbTrials is set to 1 or 2, the MAC will not decrease
+* the datarate, in case the LoRaMAC layer did not receive an acknowledgment
+*/
+uint8_t confirmedNbTrials = 4;
+
+/* Prepares the payload of the frame */
+static void prepareTxFrame( uint8_t port )
+{
+	/*appData size is LORAWAN_APP_DATA_MAX_SIZE which is defined in "commissioning.h".
+	*appDataSize max value is LORAWAN_APP_DATA_MAX_SIZE.
+	*if enabled AT, don't modify LORAWAN_APP_DATA_MAX_SIZE, it may cause system hanging or failure.
+	*if disabled AT, LORAWAN_APP_DATA_MAX_SIZE can be modified, the max value is reference to lorawan region and SF.
+	*for example, if use REGION_CN470, 
+	*the max value for different DR can be found in MaxPayloadOfDatarateCN470 refer to DataratesCN470 and BandwidthsCN470 in "RegionCN470.h".
+	*/
+  	float temperature = 12.34;  //example 22.55 *C
+  	float humidity = 76.5;    //example 72.5 %
+
+	int int_temp = temperature * 100; //remove comma
+	int int_hum = humidity * 10; //remove comma
+
+	appDataSize = 4;
+	appData[0] = int_temp >> 8;
+	appData[1] = int_temp;
+	appData[2] = int_hum >> 8;
+	appData[3] = int_hum;
+}
+
+RTC_DATA_ATTR bool firstrun = true;
+
+void setup() {
+	Serial.begin(115200);
+  Mcu.begin();
+  if(firstrun)
+  {
+    LoRaWAN.displayMcuInit();
+    firstrun = false;
+  }
+	deviceState = DEVICE_STATE_INIT;
+}
+
+void loop()
+{
+	switch( deviceState )
+	{
+		case DEVICE_STATE_INIT:
+		{
+#if(LORAWAN_DEVEUI_AUTO)
+			LoRaWAN.generateDeveuiByChipID();
+#endif
+			LoRaWAN.init(loraWanClass,loraWanRegion);
+			break;
+		}
+		case DEVICE_STATE_JOIN:
+		{
+      LoRaWAN.displayJoining();
+			LoRaWAN.join();
+			break;
+		}
+		case DEVICE_STATE_SEND:
+		{
+      LoRaWAN.displaySending();
+			prepareTxFrame( appPort );
+			LoRaWAN.send();
+			deviceState = DEVICE_STATE_CYCLE;
+			break;
+		}
+		case DEVICE_STATE_CYCLE:
+		{
+			// Schedule next packet transmission
+			txDutyCycleTime = appTxDutyCycle + randr( 0, APP_TX_DUTYCYCLE_RND );
+			LoRaWAN.cycle(txDutyCycleTime);
+			deviceState = DEVICE_STATE_SLEEP;
+			break;
+		}
+		case DEVICE_STATE_SLEEP:
+		{
+      LoRaWAN.displayAck();
+			LoRaWAN.sleep(loraWanClass);
+			break;
+		}
+		default:
+		{
+			deviceState = DEVICE_STATE_INIT;
+			break;
+		}
+	}
+}
